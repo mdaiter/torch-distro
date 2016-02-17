c8eddb98abd80dccb5653c07a094a4ea4a4fb980
Sergey Mironov <grrwlf@gmail.com>
2016-02-17 15:02:40 +0000

    Update build.nix

--- build.nix
+++ build.nix
@@ -10,13 +10,22 @@ in
 
 stdenv.mkDerivation {
   name = "torch";
+  src = ./.;
+
   buildInputs = with pkgs;
     [cmake curl readline ncurses gnuplot nodejs unzip nodePackages.npm
      libjpeg libpng imagemagick fftw sox zeromq3 qt4 pythonPackages.ipython
-     czmq openblas bash which
+     czmq openblas bash which nvidia_cg_toolkit
     ];
 
   buildCommand = ''
+    . $stdenv/setup
+    mkdir -pv $out
+    cp -r $src .
+    chown -R `whoami` */
+    chmod -R +w */
+    cd */
+    ls -lh
     bash ./install.sh
   '';
 }
